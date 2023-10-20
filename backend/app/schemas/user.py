12245669823246900c0d6b8c18315654f1f3cfa8
Tom Aitken <tclaitken@gmail.com>
2023-10-20 21:58:29 +1100

    remove erroneous fields from user schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,5 +1,4 @@
-from app.utils.core import slugify
-from pydantic import BaseModel, ConfigDict, computed_field
+from pydantic import BaseModel, ConfigDict
 
 
 class User(BaseModel):
@@ -7,24 +6,11 @@ class User(BaseModel):
 
     id: int
     username: str
+    slug: str
     email: str
     first_name: str
     last_name: str
-    reputation: int = 0
-    universal_privilege_level: str = "normal"
     is_superuser: bool = False
-
-    @computed_field  # type: ignore
-    @property
-    def encrypted_id(self) -> str:
-        from app.utils.auth import encrypt_id
-
-        return f"{encrypt_id(self.id)}"
-
-    @computed_field  # type: ignore
-    @property
-    def slug(self) -> str:
-        return slugify(self.username)
 
 class UserPrivate(User):
     hashed_password: str
